88a0a8e1bc53b6450ae463fb3a282fec867f769e
MalikaRavshanova <malikaravshanova1234@gmail.com>
2023-02-28 07:37:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// let qiymat=prompt("yoshingizni kiriting")-0
+// if(qiymat>=18){
+//     alert("katta odam")
+
+// }else if(qiymat<=10){
+//     alert("o'sha gap")
+// }
+// else{
+//     confirm("yosh bola")
+// }
+// let son=10.23339
+// console.log(Number(son.toFixed(3)))
+
+
+// const balans=prompt("son")-0
+// let kesib_tashla=Math.round(Number(balans.toFixed(3))) 
+
+// console.log(kesib_tashla)
+// const random=Math.floor(Math.random()*10)
+// console.log(random)
+// let ism=prompt("ismingizni kiriting")
+// if(ism==="lazizjon"||ism==="zaruxjon"){
+//     console.log("siz adminsiz")
+// }else{
+//     console.log("siz lazizjon emassiz")
+// }
+// const ism = prompt("ism")
+// let yosh=prompt("yosh")-0
+// if(ism==="lazizjonga"&& yosh ===14){
+//     confirm( 
+//     )
+// }oldingi dars
+
+
+// let k=15
+// let n=5
+// if(n>0){
+//     for(
+//         let i=0; i<n;i++
+//     ){
+//         console.log(k)
+//     }
+// }
+// let a=12
+// let b=20
+// if(b<a){
+//     for(
+//         let i=a; i<b;i--
+//     ){
+//         console.log(i)
+//     }
+// }
+let a=15
+let b=20
+if(a<b){
+    for(
+        let i=a+1; i<b;i++
+    ){
+        console.log("oralig'idagi sonlar"=i)
+    }
+}
+
+
